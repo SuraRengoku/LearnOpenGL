021c618cebd6848205f6e143b497216204a18f3f
jliu <jiahao.liu@rwth-aachen.de>
2023-05-16 01:04:26 +0200

    Initial Commit

---
+++ OpenGLdemo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGLdemo
+//
+//  Created by Richard Ryan on 16.05.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
