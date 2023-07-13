c5fbb4c85f48784b48a538ff6fbd0845a27d7e90
SuraRengoku <jiahao.liu@rwth-aachen.de>
2023-07-13 13:37:42 +0200

    point light initialization

---
+++ OpenGLdemo/Lighting/lightcasters/pointlight.cpp
@@ -0,0 +1,13 @@
+//
+//  pointlight.cpp
+//  OpenGLdemo
+//
+//  Created by SHERLOCK on 13.07.23.
+//
+
+#include "pointlight.hpp"
+
+
+int pointlight(){
+    return 0;
+}
---
+++ OpenGLdemo/Lighting/lightcasters/pointlight.hpp
@@ -0,0 +1,15 @@
+//
+//  pointlight.hpp
+//  OpenGLdemo
+//
+//  Created by SHERLOCK on 13.07.23.
+//
+
+#ifndef pointlight_hpp
+#define pointlight_hpp
+
+#include "shader.hpp"
+
+int pointlight();
+
+#endif /* pointlight_hpp */
