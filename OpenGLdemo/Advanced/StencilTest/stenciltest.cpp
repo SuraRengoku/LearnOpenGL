2750978452cd76d47e6e8c50ff8672f6f9a0376f
SuraRengoku <jiahao.liu@rwth-aachen.de>
2023-09-05 17:30:11 +0800

    stenciltest

---
+++ OpenGLdemo/Advanced/StencilTest/stenciltest.cpp
@@ -0,0 +1,161 @@
+//
+//  stenciltest.cpp
+//  OpenGLdemo
+//
+//  Created by SHERLOCK on 05.09.23.
+//
+
+#include "stenciltest.hpp"
+
+static unsigned const int SCR_WIDTH=800;
+static unsigned const int SCR_HEIGHT=1200;
+
+Camera camera(glm::vec3(0.0f,0.0f,6.0f));
+bool firstMouse=true;
+float lastX=SCR_WIDTH/2.0f;
+float lastY=SCR_HEIGHT/2.0f;
+
+static float deltaTime=0.0f;
+static float lastTime=0.0f;
+
+static float cubeVertices[] = {
+    // positions          // texture Coords
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+
+};
+static float planeVertices[] = {
+    // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+     5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+    -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+    -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+     5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+    -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+     5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+};
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+//这两个回调函数在glfw.h中被实现，原型为
+//typedef void (* GLFWcursorposfun)(GLFWwindow* window, double xpos, double ypos);
+//typedef void (* GLFWscrollfun)(GLFWwindow* window, double xoffset, double yoffset);
+//如果需要自定义回调函数的行为，可以在回调函数内部处理函数，但参数的类型和数量必须与库函数的要求相匹配
+
+int stenciltest(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);//设置上下文主要版本号
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//次要版本号
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //GLFW_OPENGL_PROFILE宏用于指定OpenGL上下文的配置文件类型
+    //GLFW_OPENGL_CORE_PROFILE宏用于表示使用核心配置文件
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //启用向前兼容模式，允许在较新的版本中使用较旧的特性
+#endif
+    
+    GLFWwindow* window=glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "learnOpenGL", NULL, NULL);
+    try{
+        if(window==nullptr)
+            throw std::runtime_error("window generated unsuccessfully");
+    }catch(std::runtime_error err){
+        cerr<<err.what()<<"\n";
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    try{
+        if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+            throw std::runtime_error("fail to initialize GLAD");
+    }catch(std::runtime_error err){
+        cerr<<err.what()<<"\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glEnable(GL_STENCIL_TEST);
+    /**
+     * @param GLenum sfail 模版测试失败时采取的行为
+     * @param GLenum dpfail 模版测试通过，但深度测试失败时采取的行为
+     * @param GLenum dppass 模版测试和深度测试都通过时采取的行为
+     */
+    glStencilOp(GL_KEEP,GL_KEEP,GL_REPLACE);
+    //GL_KEEP
+    //GL_ZERO
+    //GL_REPLACE
+    //GL_INCR
+    //GL_INCR_WRAP
+    //GL_DECR
+    //GL_DECR_WRAP
+    //GL_INVERT
+    
+    return 0;
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn){
+    float xpos=static_cast<float>(xposIn);
+    float ypos=static_cast<float>(yposIn);
+    
+    if(firstMouse){
+        firstMouse=false;
+        lastX=xpos;
+        lastY=ypos;
+    }
+    
+    float xoffset=xpos-lastX;
+    float yoffset=lastY-ypos;
+    lastX=xpos;
+    lastY=ypos;
+    camera.ProcessMouseMovement(xoffset, yoffset);
+    
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+    camera.ProcessMouseScroll(yoffset);
+}
+
---
+++ OpenGLdemo/Advanced/StencilTest/stenciltest.hpp
@@ -0,0 +1,15 @@
+//
+//  stenciltest.hpp
+//  OpenGLdemo
+//
+//  Created by SHERLOCK on 05.09.23.
+//
+
+#ifndef stenciltest_hpp
+#define stenciltest_hpp
+
+#include "shader.hpp"
+
+int stenciltest();
+
+#endif /* stenciltest_hpp */
